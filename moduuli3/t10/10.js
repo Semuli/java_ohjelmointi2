e2122b6c5ecefd2c4cb0ad5cc71ad6d3e0e5610a
Mikko Ilvonen <mikkoilvonen94@gmail.com>
2024-11-17 16:55:12 +0200

    moduuli3

---
+++ moduuli3/t10/10.js
@@ -0,0 +1,20 @@
+const form = document.querySelector('#source')
+const firstname = document.querySelectorAll('input')[0]
+const lastname = document.querySelectorAll('input')[1]
+const submit = document.querySelectorAll('input')[2]
+const target = document.querySelector('#target')
+
+function printnames() {
+  const fname = firstname.value
+  const lname = lastname.value
+  target.innerText = `Your name is ${fname} ${lname}`
+}
+
+form.addEventListener('click', function(event) {
+  event.preventDefault()
+})
+
+submit.addEventListener('click', function() {
+  printnames()
+})
+
