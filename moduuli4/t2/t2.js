a867bf4874ba64377736d0f894f76ec708123827
Mikko Ilvonen <mikkoilvonen94@gmail.com>
2024-11-20 15:18:04 +0200

    moduuli4

--- moduuli4/t2/t2.js
+++ moduuli4/t2/t2.js
@@ -3,13 +3,13 @@
 const form = document.querySelector('form');
 form.addEventListener('submit', async function(evt) {
   evt.preventDefault();
-  const query = document.querySelector('#query').value
+  const query = document.querySelector('#query').value;
   try {
     const response = await fetch(
         `https://api.tvmaze.com/search/shows?q=${query}`);
     const jsonData = await response.json();
     console.log(jsonData);
   } catch (error) {
-    console.log(error.message)
+    console.log(error.message);
   }
 });
